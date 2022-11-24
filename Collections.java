e4bbc242cad39f66dac0f721a24f605ca9009de2
JoannaMakalowska <joannamakalowska@gmail.com>
2022-11-24 07:46:48 -1000

    commit

--- Collections.java
+++ Collections.java
@@ -3,9 +3,34 @@ import java.util.Collection;
 
 public class Collections
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        ArrayList a = new ArrayList();
 
-        Collection a;
-        ArrayList b;
+        a.add(new String("lala"));
+        a.add(new String("lala2"));
+        a.add(new String("lala3"));
+        a.add(new String("lala4"));
+        a.add(new String("lala5"));
+        a.add(6);
+
+        for (Object stringus : a)
+            System.out.println(stringus);
+
+        Zwykla<String> nazwaZmiennej = new Zwykla<>();
+        nazwaZmiennej.nadajWartosc(("cosik"));
+        System.out.println(nazwaZmiennej.zwroc());
     }
+}
+class Zwykla <COKOLWIEK>
+{
+    public void nadajWartosc (COKOLWIEK wartosc)
+    {
+        this.nazwaZmiennejTypuCokolowiek = wartosc;
+    }
+    public COKOLWIEK zwroc ()
+    {
+        return this.nazwaZmiennejTypuCokolowiek;
+    }
+    COKOLWIEK nazwaZmiennejTypuCokolowiek;
 }
