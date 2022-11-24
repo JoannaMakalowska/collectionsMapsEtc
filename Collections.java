35568533b1653c2c9362cbec2f023a883729c223
JoannaMakalowska <joannamakalowska@gmail.com>
2022-11-23 20:39:51 -1000

    commit

---
+++ Collections.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections
+{
+    public static void main(String[] args) {
+
+        Collection a;
+        ArrayList b;
+    }
+}
